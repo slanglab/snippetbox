c0c0fc0ada7b1cc251a9cf3c90617ab40c160098
Abe Handler <akh2103@gmail.com>
2019-10-13 16:54:01 -0400

    just backup

---
+++ klm/query.py
@@ -0,0 +1,78 @@
+from __future__ import division
+
+import kenlm
+import json
+
+
+def get_unigram_probs(unigram_loc):
+    with open(unigram_loc, "r") as inf:
+        unigram_log_probs = json.load(inf)
+        unigram_log_probs = {k:float(v) for k,v in unigram_log_probs.items()}
+        return unigram_log_probs
+
+
+def slor(sequence, lm, unigram_log_probs_):
+    # SLOR function from Jey Han Lau, Alexander Clark, and Shalom Lappin
+
+    sequence = sequence.lower()
+    words = sequence.lower().split(" ")
+    p_u = sum(unigram_log_probs_[u] for u in words if u in unigram_log_probs_.keys())
+    p_u += sum(unigram_log_probs_['<unk>'] for u in words if u not in unigram_log_probs_.keys())
+
+    p_m = lm.score(sequence)
+
+    len_s = len(words) + 0.0
+
+    return (p_m - p_u)/len_s
+
+
+class LM:
+
+    def __init__(self, loc):
+
+        self.model = kenlm.LanguageModel(loc)
+
+    def score(self, str_):
+        # str_ is a " "-delimited string, e.g. "I am a student"
+
+        return self.model.score(str_)
+
+
+if __name__ == "__main__":
+
+    from environments.envs import ENVIRONMENTS
+    import argparse
+    parser = argparse.ArgumentParser()  # pylint: disable=invalid-name
+    parser.add_argument('-e', '--environment', type=str, default="DEV") 
+
+    args = parser.parse_args()
+
+    env = ENVIRONMENTS[args.environment.upper()]
+    LOC = env['klm_model']
+    UNIGRAM_LOC = env['klm_model']
+    UG_MODEL = env["ug_model"]
+
+    lm = LM(loc=LOC)
+    up = get_unigram_probs(UG_MODEL)
+
+    print("[*] slor: I am a student")
+    print(slor("I am a student", lm, up))
+
+    print("[*] slor: I a student")
+    print(slor("I a student", lm, up))
+    
+    print("[*] slor: student I am")
+    print(slor("student i am", lm, up))
+
+
+    print("[*] slor: American troops")
+    print(slor("American troops", lm, up))
+
+    print("[*] slor: American troops landed")
+    print(slor("American troops landed", lm, up))
+
+    print("[*] slor: American troops gave")
+    print(slor("American troops gave", lm, up))
+
+    print("[*] slor: American troops gave gifts")
+    print(slor("American troops gave gifts", lm, up))
