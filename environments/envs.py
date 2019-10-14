8f6c3a9114cc9d395d9e0eb42464f66315403d7f
Abe Handler <akh2103@gmail.com>
2019-10-14 13:56:47 -0400

    just backup

---
+++ environments/envs.py
@@ -0,0 +1,10 @@
+
+DEV = {
+        'klm_model': "/Users/ahandler/research/tmp_rl_idea/klm/fa.klm",
+        'ug_model': "/Users/ahandler/research/tmp_rl_idea/klm/fa.unigrams.json"
+}
+
+
+ENVIRONMENTS = {
+  'DEV': DEV
+}
