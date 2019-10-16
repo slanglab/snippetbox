912b7612e0e4c08c12eef1e5baace356c27a66ed
Abe Handler <akh2103@gmail.com>
2019-10-16 14:05:05 -0400

    just backup

---
+++ nb.sh
@@ -0,0 +1,1 @@
+python snippetbox/lib/python3.7/site-packages/jupyter.py notebook
--- scripts/preprocess_civic.py
+++ scripts/preprocess_civic.py
@@ -5,12 +5,14 @@ Preprocess the community crit dataset using spacy and phrasemachine
 import spacy
 import json
 import phrasemachine
-
+from spacy.pipeline import Sentencizer
+sentencizer = Sentencizer()
 
 def preprocess():
 
     nlp = spacy.load("en_core_web_sm")
-
+    nlp.add_pipe(sentencizer, before="parser") 
+    
     with open("corpora/data_ccrit.json", "r") as inf:
         comments = json.load(inf)
 
@@ -23,6 +25,9 @@ def preprocess():
 
             tokens = [token.text for token in doc]
             pos = [token.pos_ for token in doc]
+            
+            #tok spans for sentences
+            sentences = [(o.start, o.end) for o in doc.sents]
 
             # need minlen=1 here b/c bigger phrases are sparse
             phrases = phrasemachine.get_phrases(minlen=1,
@@ -35,4 +40,10 @@ def preprocess():
 
             comment["docid"] = docid
 
+            comment["sentences"] = sentences
+
             of.write(json.dumps(comment) + "\n")
+
+
+if __name__ == "__main__":
+    preprocess()
